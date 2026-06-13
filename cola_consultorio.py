5904910a681280cc58e08b9bd3ddf9153cc5bced
elfokinyuyu666 <gcmich3@gmail.com>
2026-06-13 09:26:53 -0600

    Fix constructor method name in ColaConsultorio class

--- cola_consultorio.py
+++ cola_consultorio.py
@@ -1,27 +1,21 @@
 import time
+
 class ColaConsultorio:
 
-
-    def __init__(self):
+    def _init_(self):
         self.cola = []
-
 
     def agregar_paciente(self, paciente):
         self.cola.append(paciente)
-
 
     def atender_paciente(self):
         if len(self.cola) > 0:
             return self.cola.pop(0)
         return None
 
-
-    # AQUÍ PEGAS LA FUNCIÓN
     def iniciar_simulacion(self):
         while len(self.cola) > 0:
-
             print("\n===== ATENDIENDO PACIENTE =====")
-
             paciente = self.atender_paciente()
 
             print("Paciente:", paciente.get__nombre())
@@ -32,6 +26,4 @@ class ColaConsultorio:
 
             print("Consulta terminada...")
             print("Esperando siguiente paciente...")
-
             time.sleep(5)
-   
