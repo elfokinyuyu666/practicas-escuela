97ebea5f413f3fd15f7ad24dfe12a782af715809
elfokinyuyu666 <gcmich3@gmail.com>
2026-06-13 00:55:42 -0600

    Refactor Medico class constructor and methods
    Updated constructor to initialize attributes from Persona class and fixed method names.

--- Clase_medico.py
+++ Clase_medico.py
@@ -5,13 +5,13 @@ class Medico(Persona):
     def __init__(self, nombre, edad, sexo, telefono, correo,
                  especialidad, consultorio, num_empleado):
 
-        super().__init__()
+        super().__init__(nombre, edad, sexo, telefono, correo)
 
-        self.set__nombre(nombre)
-        self.set__edad(edad)
-        self.set__sexo(sexo)
-        self.set__telefono(telefono)
-        self.set__correo(correo)
+        self.set_nombre(nombre)
+        self.set_edad(edad)
+        self.set_sexo(sexo)
+        self.set_telefono(telefono)
+        self.set_correo(correo)
 
         self.especialidad = especialidad
         self.consultorio = consultorio
@@ -19,11 +19,8 @@ class Medico(Persona):
 
     def mostrar_datos(self):
         print("===== DATOS DEL MÉDICO =====")
-        print("Nombre:", self.get__nombre())
-        print("Edad:", self.get__edad())
-        print("Sexo:", self.get__sexo())
-        print("Teléfono:", self.get__telefono())
-        print("Correo:", self.get__correo())
-        print("Especialidad:", self.especialidad)
-        print("Consultorio:", self.consultorio)
-        print("Número de empleado:", self.num_empleado)
+        print("Nombre:", self.get_nombre())
+        print("Edad:", self.get_edad())
+        print("Sexo:", self.get_sexo())
+        print("Teléfono:", self.get_telefono())
+        print("Correo:", self.get_correo())
