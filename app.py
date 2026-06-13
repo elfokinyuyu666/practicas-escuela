6e4adbc828cd76519009b34c247c95fc5e8073b4
elfokinyuyu666 <gcmich3@gmail.com>
2026-06-13 01:16:54 -0600

    Refactor app.py to restore original functionality

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ if st.session_state.running:
 
         st.success(f"👤 Atendiendo: {atendido.get_nombre()}")
         st.write("🤒", atendido.padecimiento)
-        st.write("👨‍⚕️", atendido.medico.nombre)
+        st.write("👨‍⚕️", atendido.medico. get_nombre())
         st.write("🏥", atendido.medico.consultorio)
         st.write("⚖️ IMC:", round(atendido.calcular_imc(), 2))
 
