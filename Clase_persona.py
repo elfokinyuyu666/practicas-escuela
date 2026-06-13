41cf4042650d95704314b61b8f78bbfd5709989c
elfokinyuyu666 <gcmich3@gmail.com>
2026-06-13 12:11:45 -0600

    Refactor Persona class with consistent formatting

--- Clase_persona.py
+++ Clase_persona.py
@@ -1,26 +1,26 @@
 class Persona:
 
     def _init_(self):
-        self.__nombre=""
-        self.__edad=0
-        self.__sexo=""
-        self.__telefono=""
-        self.__correo=""
+        self.__nombre = ""
+        self.__edad = 0
+        self.__sexo = ""
+        self.__telefono = ""
+        self.__correo = ""
 
-    def set__nombre(self,no):
-        self.__nombre=no
- 
-    def set__edad(self,e):
-        self.__edad=e
+    def set__nombre(self, no):
+        self.__nombre = no
 
-    def set__sexo(self,s):
-        self.__sexo=s
+    def set__edad(self, e):
+        self.__edad = e
 
-    def set__telefono(self,t):
-        self.__telefono=t
+    def set__sexo(self, s):
+        self.__sexo = s
 
-    def set__correo(self,co):
-        self.__correo=co
+    def set__telefono(self, t):
+        self.__telefono = t
+
+    def set__correo(self, co):
+        self.__correo = co
 
     def get__nombre(self):
         return self.__nombre
@@ -33,6 +33,6 @@ class Persona:
 
     def get__telefono(self):
         return self.__telefono
-    
+
     def get__correo(self):
         return self.__correo
