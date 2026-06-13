506c79cd6ed47d075014ea63c29c56279c7560ba
elfokinyuyu666 <gcmich3@gmail.com>
2026-06-13 01:54:38 -0600

    Fix attribute access in Medico class
    Updated method calls to use double underscores for attribute access.

--- Clase_medico.py
+++ Clase_medico.py
@@ -1,4 +1,5 @@
 from Clase_persona import Persona
+
 class Medico(Persona):
 
     def __init__(self, nombre, edad, sexo, telefono, correo,
@@ -6,26 +7,23 @@ class Medico(Persona):
 
         super().__init__()
 
-        self.set_nombre(nombre)
-        self.set_edad(edad)
-        self.set_sexo(sexo)
-        self.set_telefono(telefono)
-        self.set_correo(correo)
+        self.set__nombre(nombre)
+        self.set__edad(edad)
+        self.set__sexo(sexo)
+        self.set__telefono(telefono)
+        self.set__correo(correo)
 
         self.especialidad = especialidad
         self.consultorio = consultorio
         self.num_empleado = num_empleado
 
     def mostrar_datos(self):
-       print("===== DATOS DEL MÉDICO =====")
-       print("Nombre:", self.get_nombre())
-       print("Edad:", self.get_edad())
-       print("Sexo:", self.get_sexo())
-       print("Teléfono:", self.get_telefono())
-       print("Correo:", self.get_correo())
-       print("Especialidad:", self.especialidad)
-       print("Consultorio:", self.consultorio)
-       print("Número de empleado:", self.num_empleado)
-       print("Especialidad:", self.especialidad)
-       print("Consultorio:", self.consultorio)
-       print("Número de empleado:", self.num_empleado)
+        print("===== DATOS DEL MÉDICO =====")
+        print("Nombre:", self.get__nombre())
+        print("Edad:", self.get__edad())
+        print("Sexo:", self.get__sexo())
+        print("Teléfono:", self.get__telefono())
+        print("Correo:", self.get__correo())
+        print("Especialidad:", self.especialidad)
+        print("Consultorio:", self.consultorio)
+        print("Número de empleado:", self.num_empleado)m_empleado)
